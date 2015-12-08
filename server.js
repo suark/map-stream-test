e856f9770c324a589e733c13824ddeea438bf722
Brian Kraus <suark.nairb@gmail.com>
2015-12-08 12:06:37 -0700

    Ready to write the actual data sending functionality

--- server.js
+++ server.js
@@ -4,75 +4,58 @@ var server = restify.createServer({
 	name: 'MapStream'
 });
 
-// server.get('/test', function(req, res, next) {
-
-// 	res.header('Content-Type', 'application/json');
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-
-// 	res.send('{"thing1":"hello","thing2":0}');
-// });
+/*This data recipe thing is just my goofy way of saying, 
+I want X features that take Y seconds to get, and so on.*/
+var data_recipe = [{
+	time: 15,
+	amount: 10
+}, {
+	time: 10,
+	amount: 20
+}, {
+	time: 8,
+	amount: 30
+}, {
+	time: 5,
+	amount: 40
+}, {
+	time: 2,
+	amount: 50
+}, {
+	time: 1,
+	amount: 100
+}];
 
 server.get('/test', function(req, res, next) {
-	res.send(200, {
+	res.send({
 		'thing1': 'hello',
 		'thing2': 0
 	});
-	// return next();
+	return next();
+});
+
+server.get('/fetch', function(req, res, next) {
+	res.send({
+		'thing1': 'hello',
+		'thing2': 0
+	});
+	return next();
 });
 
 // Quick route to send an sample number of pairs over a period of time
-server.get('/pairs', function(req, res, next) {
+server.get('/stream', function(req, res, next) {
 	numSent = 0;
 
 	// Send Header
 	res.header('Content-Type', 'application/json');
 	res.header('Access-Control-Allow-Origin', '*');
 
-	// // Write a number of pairs between 1 and 5 to the response
-	// // If flag is true, end the communication
-	// function sendPairs(res, flag) {
-	// 	var numPairs = Math.floor((((Math.random() * 10) % 5) + 1));
-	// 	console.log('Number of Pairs: ' + (numPairs + 1));
+	res.write({
+		'thing1': 'hello',
+		'thing2': 0
+	});
+	res.end();
 
-	// 	for (var i = 0; i <= numPairs; i++) {
-
-	// 		// Generate the pair
-	// 		var pair = {
-	// 			x: Math.floor(((Math.random() * 1000) % 100) + 1),
-	// 			y: Math.floor(((Math.random() * 1000) % 100) + 1)
-	// 		};
-
-	// 		// Write the pair
-	// 		res.write(JSON.stringify(pair) + ',');
-	// 		numSent++;
-
-	// 		// End if true
-	// 		if (flag) {
-	// 			res.write('{"x":0, "y": 0}]}');
-	// 			res.end();
-	// 			console.log('Sent: ' + numSent);
-	// 			return next();
-	// 		}
-	// 	}
-
-	// 	// not ending
-	// 	return flag;
-	// }
-
-	// // Start array of pairs
-	// res.write('{ "data": [');
-
-	// // Queue timeouts
-	// for (var i = 0; i < 1000; i++) {
-	// 	var seconds = Math.floor(((Math.random() * 10000) % 7000) + 50);
-	// 	console.log('Waiting: ' + seconds);
-
-	// 	setTimeout(sendPairs, seconds, res, false);
-	// }
-
-	// // Last Pair sent, all others must finish before this to be recieved by client
-	// setTimeout(sendPairs, 7100, res, true);
 });
 
 server.get('/.*/', restify.serveStatic({
