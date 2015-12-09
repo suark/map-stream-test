8e9a43aeae9bed6726795a69a76b15f86d11aa3c
Brian Kraus <suark.nairb@gmail.com>
2015-12-09 00:52:59 -0700

    Removed a bunch of bloat

--- public/client.js
+++ public/client.js
@@ -1,113 +1,88 @@
-//This is our app
-var app = {
+console.log("App Initiated");
 
-	init: function(options) {
-		/*
-			Start the app. 
-			options.container: References the app's main dom element 
-		*/
-		console.log("App Initiated");
+var url = 'http://localhost:3001/features';
 
-		this.options = options;
-		this.options.url = 'http://localhost:3001/features';
-		//Create our LeafletJS Map
-		var map = this.createMap();
+createMap();
 
-		this.setupControls();
 
-	},
 
-	setupControls: function() {
-		var self = this;
-		/*
-			Setup any buttons or other controls
-		*/
-		var requestTraditional = $('#traditionalButton');
-		var requestStreaming = $('#streamButton');
 
-		requestTraditional.click(function() {
-			self.requestTraditional();
+//Functions ~~~~
+
+function requestTraditional() {
+	/*
+		Makes a request to fetch features using a traditional request.
+		Each fetched feature is passed to 'displayFeature'
+	*/
+	console.log('Requesting Features Traditionally')
+
+	oboe(url)
+		.done(function(data) {
+			console.log(data);
+		})
+		.fail(function() {
+			console.log('Didn\'t reveive data.')
 		});
-		requestStreaming.click(function() {
-			self.requestStreaming();
+};
+
+function requestStreaming() {
+	/*
+		Makes a request to fetch features using streaming.
+		Each fetched feature is passed to 'displayFeature'
+	*/
+	console.log('Requesting Features with Streaming')
+
+	oboe(url)
+		.node('things.*', function(thing) {
+			console.log(thing);
+		})
+		.node('points.*', function(thing) {
+			console.log(thing);
+
+		})
+		.node('polygons.*', function(thing) {
+			console.log(thing);
+
+		})
+		.done(function(things) {
+			console.log(things);
 		});
-	},
+};
 
-	requestTraditional: function() {
-		/*
-			Makes a request to fetch features using a traditional request.
-			Each fetched feature is passed to 'displayFeature'
-		*/
-		console.log('Requesting Features Traditionally')
+function displayFeature(feature) {
+	/*
+		Take a feature and attempts to add it to the map
+	*/
+	console.log('Displaying Features: ', feature);
+};
 
-		oboe(this.options.url)
-			.done(function(data) {
-				console.log(data);
-			})
-			.fail(function() {
-				console.log('Didn\'t reveive data.')
-			});
-	},
+function createMap() {
+	/*
+		Create our Leaflet Map.
+		This assumes that a div exists with the id="map"
+	*/
+	console.log('Creating the Map')
 
-	requestStreaming: function() {
-		/*
-			Makes a request to fetch features.
-			Each fetched feature is passed to 'displayFeature'
-		*/
-		console.log('Requesting Features with Streaming')
+	var map = L.map('map', {
+		center: [53.579, -109.072],
+		zoom: 5
+	});
 
-		oboe(this.options.url)
-			.node('things.*', function(thing) {
-				console.log(thing);
-			})
-			.node('points.*', function(thing) {
-				console.log(thing);
+	var baseMaps = {
+		OpenStreetMap: L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			// attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}),
+		'ESRI Arial': L.tileLayer("//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg", {
+			maxZoom: 18,
+			// attribution: 'Tiles Â© Esri â€” Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+		})
+	};
 
-			})
-			.node('polygons.*', function(thing) {
-				console.log(thing);
+	L.control.layers(baseMaps, null, {
+		position: 'topleft'
+	}).addTo(map);
 
-			})
-			.done(function(things) {
-				console.log(things);
-			});
-	},
+	baseMaps['ESRI Arial'].addTo(map);
 
-	displayFeature: function(feature) {
-		/*
-			Take a feature and attempts to add it to the map
-		*/
-		console.log('Displaying Features: ', feature);
-	},
-
-	createMap: function() {
-		/*
-			Create our Leaflet Map.
-			This assumes that a div exists with the id="map"
-		*/
-		console.log('Creating the Map')
-
-		var map = L.map('map', {
-			center: [53.579, -109.072],
-			zoom: 5
-		});
-
-		var baseMaps = {
-			OpenStreetMap: L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-				// attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-			}),
-			'ESRI Arial': L.tileLayer("//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg", {
-				maxZoom: 18,
-				// attribution: 'Tiles Â© Esri â€” Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-			})
-		};
-
-		L.control.layers(baseMaps, null, {
-			position: 'topleft'
-		}).addTo(map);
-
-		baseMaps['ESRI Arial'].addTo(map);
-
-		return map;
-	}
+	return map;
 };
